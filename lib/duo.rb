de9fde97569a0f3cdc4aabff5215a475b0ac92a8
Andrew Walter <andrew.walter@burnet.edu.au>
2022-08-19 13:37:34 +1000

    Initial commit

---
+++ lib/duo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "duo/version"
+
+module Duo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/duo/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Duo
+  VERSION = "0.1.0"
+end
